9b002d0ee415c5bc802f2f1bc8b06e37d09df859
xxxOverSide <postolenko00@bk.ru>
2024-02-12 20:01:04 +0300

    1

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-from aiogram import Dispatcher, Bot, types,F
-from aiogram.filters.command import Command
-from aiogram.enums import ParseMode
-import asyncio, logging
+import asyncio
+from aiogram import Dispatcher, Bot, types
 from settings import BOT_TOKEN
 
-
-logging.basicConfig(level=logging.INFO)
 
 bot = Bot(BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
 
+@dp.message()
+async def start_cmd(msg: types.Message):
+    await msg.answer("Hello!")
 
-#отправляет сообщения по команде
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello")
-
-
-#Отвечает на сообщения по команде
-@dp.message(Command("test1"))
-async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
 
 async def main():
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
