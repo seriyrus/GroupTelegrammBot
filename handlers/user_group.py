acbe3f86c94727fc103cfee3af32ab94c837b034
xxxOverSide <postolenko00@bk.ru>
2024-02-13 18:53:31 +0300

    restricted_words

--- handlers/user_group.py
+++ handlers/user_group.py
@@ -7,7 +7,12 @@ from string import punctuation
 user_group_router = Router()
 
 
-restricted_words = {'недоразвитый',"пидр", "гей","тварь"}
+restricted_words = {'недоразвитый',"пидр", "гей","тварь",'блядь', 
+'ебать', 'хуй', 'пизда', 'нахуй', 'сука', 'ебаный', 'ебучий', 'ебало', 'ебло', 
+'шлюха','шалава','шлюхи','завались','блять','пидорас','еблан','ублюдок','хуесос',
+'нахуя','схуяли','похую','по хую','заебало','сдохну','сдохни','сдохнуть','убейся',
+'ахуе','бля','пиздос','егор','гандон','олег','макс','геля','охуел','серега','чмо','ебал','','','','','','',''
+}
 
 def clean_text(text: str):
     return text.translate(str.maketrans('','',punctuation))
@@ -16,4 +21,5 @@ def clean_text(text: str):
 async def start_cmd(msg: types.Message):
     if restricted_words.intersection(msg.text.lower().split()):
         await msg.delete()
+        await msg.answer("Ай-ай по попке")
         #await msg.chat.ban(msg.from_user.id)
